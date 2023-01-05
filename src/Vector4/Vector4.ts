fd35186bf34e48e0d0472dce882a9e2f85a7c733
bad4iz <developer@bad4iz.ru>
2023-01-05 16:17:15 +0400

    jsdoc

--- src/Vector4/Vector4.ts
+++ src/Vector4/Vector4.ts
@@ -1,22 +1,32 @@
 /**
- * @typedef {} Float
+ * Тип пропросов массива из четырех элементов.
+ *
+ * @example ------
+ * const arr: Array4 = [1, 2, 1, 0]
  */
+export type Array4 = readonly [number, number, number, number];
 
 /**
- * Vector4
- * __Вектор из четырех элементов.__
- *
+ * Vector4 __Вектор из четырех элементов.__
  * Если указан opt_src, новый вектор инициализируется opt_src.
  * If opt_src is specified, new vector is initialized by opt_src.
  *
- * @class
- * @param {[Float, Float, Float, Float]} [opt_src] - Source vector(option).
+ * @example ------
+ * new Vector4();
  */
 class Vector4 {
   readonly elements: Float32Array;
-  constructor(opt_src: [number, number, number, number] = [0, 0, 0, 0]) {
+
+  /**
+   * Constructor.
+   *
+   * @param {Array4} [opt_src=[0, 0, 0, 0]] - The source vector.
+   */
+  constructor(opt_src: Array4 = [0, 0, 0, 0]) {
     this.elements = new Float32Array(opt_src.slice(0, 4));
   }
 }
 
 export { Vector4 };
+
+new Vector4();
